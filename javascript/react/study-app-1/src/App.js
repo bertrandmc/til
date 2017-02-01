011dd5bc302c3e3a9f397370ac9834b7073564f2
bema <bema@bemas-MacBook-Pro.local>
2017-02-01 17:24:42 +0000

    Update App Title

--- javascript/react/study-app-1/src/App.js
+++ javascript/react/study-app-1/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Tools</h2>
+          <h2>React Playground</h2>
         </div>
         <TodoApp />
       </div>
