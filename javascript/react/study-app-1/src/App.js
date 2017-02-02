a2ecd7e829d9a6e6241308713250bd5b9f5fe449
bema <bema@bemas-MacBook-Pro.local>
2017-02-02 12:57:00 +0000

    Wrap todo app in DeviceWrapper component

--- javascript/react/study-app-1/src/App.js
+++ javascript/react/study-app-1/src/App.js
@@ -3,16 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 import TodoApp from './components/todo';
+import DeviceWrapper from './components/device-wrapper';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="app">
+        <div className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
           <h2>React Playground</h2>
         </div>
-        <TodoApp />
+
+        <div className="app-wrapper">
+          <DeviceWrapper type="mobile">
+            <TodoApp />
+          </DeviceWrapper>
+        </div>
+
       </div>
     );
   }
