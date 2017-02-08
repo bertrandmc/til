b1f57f13553d595afd2b0d33a7ae66dc20c70027
bema <bema@bemas-MacBook-Pro.local>
2017-02-08 11:07:17 +0000

    Implement Todo dueDate and note fields

--- javascript/react/study-app-1/src/components/todo/components/Todo/Todo.js
+++ javascript/react/study-app-1/src/components/todo/components/Todo/Todo.js
@@ -1,17 +1,21 @@
 import React, {Component} from 'react';
 import debounce from 'lodash/debounce';
 import classNames from 'classnames';
+import moment from 'moment';
 
 import './todo.css';
-
+import DatePicker from '../Inputs/DatePicker';
+import IconCalendar from 'react-icons/lib/md/event';
+import IconPencil from 'react-icons/lib/fa/pencil';
 import ArrowLeft from 'react-icons/lib/md/keyboard-arrow-left';
-import {
-  ButtonRemove
-} from '../';
+import { ButtonRemove } from '../';
+
 
 
 export class Todo extends Component {
-  state = {}
+  state = {
+    showDatePicker: false
+  }
 
   constructor(props) {
     super(props);
@@ -24,7 +28,10 @@ export class Todo extends Component {
       // back button called
       // allow 400ms for animation before removing it from state
       setTimeout(() => {
-        this.setState({todo: nextProps.todo});
+        this.setState({
+          todo: nextProps.todo,
+          activateDatePicker: false
+        });
       }, 400);
     } else {
       // todo was selected and shoudl render immediatelly
@@ -40,13 +47,37 @@ export class Todo extends Component {
     });
   }
 
-  handleTitleChange = (event) => {
+  handleInputChange = (propName, event) => {
     event.preventDefault();
-    this.handleSave('title', event.target.value);
+    this.handleSave(propName, event.target.value);
+  }
+
+  handleDueDate = (date) => {
+    this.handleSave('dueDate', date.format());
+    this.toggleDatePicker();
+  }
+
+  toggleDatePicker = () => {
+    this.setState((prevState) => ({
+      showDatePicker: !prevState.showDatePicker,
+      activateDatePicker: true
+    }));
+  }
+
+  getFriendlyDate(date) {
+    if(!date) return;
+    return moment(date).calendar(null, {
+      lastDay: '[Yesterday]',
+      sameDay: '[Today]',
+      nextDay: '[Tomorrow]',
+      lastWeek: 'dddd Do',
+      nextWeek: 'dddd Do',
+      sameElse: 'MMMM Do'
+    });
   }
 
   render() {
-    const { todo } = this.state;
+    const { todo, showDatePicker, activateDatePicker } = this.state;
 
     return (
       <div className={classNames("todo", this.props.className)}>
@@ -57,13 +88,30 @@ export class Todo extends Component {
               <input type="text"
                 className="todo-header-title-input"
                 value={todo.title}
-                onChange={this.handleTitleChange} />
+                onChange={event => this.handleInputChange('title', event)} />
+            </div>
+
+            <div className="todo-prop" onClick={this.toggleDatePicker}>
+              <IconCalendar className="todo-prop-icon" />
+              <div>{this.getFriendlyDate(todo.dueDate)}</div>
+            </div>
+
+            <div className="todo-prop todo-prop-comment">
+              <IconPencil className="todo-prop-icon" />
+              <div>
+                  <textarea rows="5"
+                    value={todo.notes}
+                    onChange={event => this.handleInputChange('notes', event)}></textarea>
+              </div>
             </div>
 
             <div className="todo-actions">
               <ButtonRemove handleRemoveTodo={this.props.handleRemoveTodo} todo={todo} />
-              <ButtonRemove handleRemoveTodo={this.props.handleRemoveTodo} todo={todo} />
             </div>
+
+            {activateDatePicker &&
+              <DatePicker isVisible={showDatePicker} selectedDate={todo.dueDate} handleChange={this.handleDueDate} />
+            }
           </span>
         }
       </div>
